7389e8bf3bf566ebf6104a4383d217dece58a38a
Foool1 <julek.pryczkowski@wp.pl>
2025-06-09 16:23:00 +0200

    Created Task Model

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'tasks',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- app/core/models.py
+++ app/core/models.py
@@ -1,2 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+
+
+
+class Task(models.Model):
+
+    status_choices = [
+        ('Nowy', 'Nowy'),
+        ('W toku', 'W toku'),
+        ('Rozwiązany', 'Rozwiązany'),
+    ]
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(choices=status_choices, default="Nowy")
+    assigned_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='assigned_tasks'
+    )
+
+
+    def __str__(self):
+        return f"{self.name} (status: {self.status})"
